7b570650988dae0aacf77efb7c1274b91bfc0c54
Simon Bennett <simon@bennett.im>
2016-08-10 21:08:46 +0100

    sda

--- install.sh
+++ install.sh
@@ -122,7 +122,7 @@ echo "
 timeout 5
 title ZeroShell $ZEROSHELL ($KERNEL with Xen support)
 root (hd0)
-kernel /boot/vmlinuz-$KERNEL-ZS root=/dev/xvda rw
+kernel /boot/vmlinuz-$KERNEL-ZS root=/dev/sda rw
 initrd /boot/initrd.img
 " > "$ZEROSHELL_DISK/boot/grub/menu.lst"
 mkdir "$ZEROSHELL_DISK/initrd" || true
